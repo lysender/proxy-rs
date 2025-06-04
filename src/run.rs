743db2c511cb31861e453763aca8553658ec8305
Lysender <lysender@darkstar-win>
2025-06-04 19:02:10 +0800

    fix: add shutdown handler

--- src/run.rs
+++ src/run.rs
@@ -57,8 +57,34 @@ pub async fn run(config: Config) -> Result<()> {
 
     let listener = TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, routes_all.into_make_service())
+        .with_graceful_shutdown(shutdown_signal())
         .await
         .unwrap();
 
+    info!("HTTP server stopped");
+
     Ok(())
+}
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
 }
